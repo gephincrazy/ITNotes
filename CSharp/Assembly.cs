4bd16b5d40f19ec51a61592f3f75edb419bc122a
joseph.xu <gephincrazy@gmail.com>
2017-02-17 14:37:27 +0800

    update'

---
+++ CSharp/Assembly.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace QueryLog4Net
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var result = DoWorkAsync().Result;
+            Console.WriteLine(result);
+            Console.ReadLine();
+        }
+
+        static Task<string> DoWorkAsync()
+        {
+            return Task<string>.Factory.StartNew(() => {
+                Thread.Sleep(3000);
+                return "Hello world...";
+            });
+        }
+
+
+        static void GetLog()
+        {
+            string directory = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+            string file = directory.Substring(0, directory.IndexOf("bin")) + "loginfo.txt";
+            string text = File.ReadAllText(file);
+            string outtext = "";
+            // Define a regular expression for repeated words.
+            Regex rx = new Regex(@"https://www.booking.com/hotel/[\w]+/[\w\-]+.zh-cn.html", RegexOptions.Compiled | RegexOptions.IgnoreCase);
+
+            // Find matches.
+            MatchCollection matches = rx.Matches(text);
+
+            // Report on each match.
+            foreach (Match match in matches)
+            {
+                GroupCollection groups = match.Groups;
+                Console.WriteLine(groups[0].Value);
+                outtext = outtext + groups[0].Value + "\n";
+            }
+
+            File.WriteAllText(@"D:\VS2015Project\QueryLog4Net\QueryLog4Net\logresult.txt", outtext);
+            Console.ReadKey();
+        }
+
+    }
+}
--- SQL/LinkServer.sql
+++ SQL/LinkServer.sql
@@ -4,12 +4,8 @@ EXEC sp_helpserver			-- Also shows services etc.
 EXEC sp_dropserver  @server = 'MyRemoteServer',  @droplogins = 'droplogins'		-- 'droplogins' = Drop associated logins, NULL=Do not drop logins
 
 -- Create Linked Server (use command below for NON-Standard port
-EXEC sp_addlinkedserver  @server = 'MyRemoteServer'	,	@srvproduct = 'SQL Server'	,@provider = 'SQLOLEDB'	 ,@datasrc = 'MyRemoteServer'	
-
--- For NON-Standard SQL Port use:
-EXEC sp_addlinkedserver @server = 'MyRemoteServer', @srvproduct = 'SQL Server', @provider = 'SQLOLEDB', @datasrc = 'MyRemoteServer,1144'	-- Set the port appropriately
--- Example of Oracle connection:
-EXEC sp_addlinkedserver @server = 'MyOracleServer', @srvproduct = 'Oracle', @provider = 'MSDAORA', @datasrc = 'OracleServerName'	-- , @location=NULL, @provstr=NULL, @catalog=NULL
+EXEC master.dbo.sp_addlinkedserver @server = N'139.224.190.159', @srvproduct=N'SQL Server'
+EXEC master.dbo.sp_addlinkedsrvlogin @rmtsrvname=N'139.224.190.159',@useself=N'False',@locallogin=NULL,@rmtuser=N'iZhd7m11gnjt8eZ',@rmtpassword='########'
 
 -- Remove existing Linked Server Login (optional)
 EXEC sp_droplinkedsrvlogin @rmtsrvname = 'MyRemoteServer',	@locallogin = 'MyUserID'
@@ -25,5 +21,39 @@ exec sp_serveroption 'MyRemoteServer', 'data access', 'true'
 
 -- Alternative test using OPENQUERY
 SELECT	* FROM	OPENQUERY([MyRemoteServer], 'SELECT TOP 10 * FROM master.dbo.sysobjects') 
+
+
+GO
+
+
+
+
+GO
+
+EXEC master.dbo.sp_serveroption @server=N'139.224.190.159', @optname=N'collation compatible', @optvalue=N'false'
+GO
+EXEC master.dbo.sp_serveroption @server=N'139.224.190.159', @optname=N'data access', @optvalue=N'true'
+GO
+EXEC master.dbo.sp_serveroption @server=N'139.224.190.159', @optname=N'dist', @optvalue=N'false'
+GO
+EXEC master.dbo.sp_serveroption @server=N'139.224.190.159', @optname=N'pub', @optvalue=N'false'
+GO
+EXEC master.dbo.sp_serveroption @server=N'139.224.190.159', @optname=N'rpc', @optvalue=N'false'
+GO
+EXEC master.dbo.sp_serveroption @server=N'139.224.190.159', @optname=N'rpc out', @optvalue=N'false'
+GO
+EXEC master.dbo.sp_serveroption @server=N'139.224.190.159', @optname=N'sub', @optvalue=N'false'
+GO
+EXEC master.dbo.sp_serveroption @server=N'139.224.190.159', @optname=N'connect timeout', @optvalue=N'0'
+GO
+EXEC master.dbo.sp_serveroption @server=N'139.224.190.159', @optname=N'collation name', @optvalue=null
+GO
+EXEC master.dbo.sp_serveroption @server=N'139.224.190.159', @optname=N'lazy schema validation', @optvalue=N'false'
+GO
+EXEC master.dbo.sp_serveroption @server=N'139.224.190.159', @optname=N'query timeout', @optvalue=N'0'
+GO
+EXEC master.dbo.sp_serveroption @server=N'139.224.190.159', @optname=N'use remote collation', @optvalue=N'true'
+GO
+EXEC master.dbo.sp_serveroption @server=N'139.224.190.159', @optname=N'remote proc transaction promotion', @optvalue=N'true'
 GO
 
